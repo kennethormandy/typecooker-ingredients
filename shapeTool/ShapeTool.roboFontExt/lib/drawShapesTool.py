30b2db98bb7cbdf9970ce589887b45f8d2fba290
Frederik Berlaen <frederik@typemytype.com>
2014-08-19 10:18:00 +0200

    control to move the shape around

--- shapeTool/ShapeTool.roboFontExt/lib/drawShapesTool.py
+++ shapeTool/ShapeTool.roboFontExt/lib/drawShapesTool.py
@@ -26,36 +26,36 @@ class GeometricShapesWindow(object):
     """
     The Modal window that allows numbers input to draw basic geometric shapes.
     """
-    
+
     def __init__(self, glyph, callback, x, y):
         self.glyph = glyph
         self.callback = callback
         ## create the modal dialog (from dialogKit)
-        self.w = ModalDialog((200, 150), 
-                            "Shapes Tool", 
-                            okCallback=self.okCallback, 
+        self.w = ModalDialog((200, 150),
+                            "Shapes Tool",
+                            okCallback=self.okCallback,
                             cancelCallback=self.cancelCallback)
-        
+
         ## add some text boxes
         self.w.xText = TextBox((10, 13, 100, 22), "x")
         self.w.yText = TextBox((10, 43, 100, 22), "y")
         self.w.wText = TextBox((100, 13, 100, 22), "w")
         self.w.hText = TextBox((100, 43, 100, 22), "h")
-        
+
         ## adding input boxes
         self.w.xInput = EditText((30, 10, 50, 22), "%i" %x)
         self.w.yInput = EditText((30, 40, 50, 22), "%i" %y)
         self.w.wInput = EditText((120, 10, 50, 22))
         self.w.hInput = EditText((120, 40, 50, 22))
-        
-        ## a radio shape choice group 
+
+        ## a radio shape choice group
         ## (the RadioGroup isn't standaard in dialogKit, this is a vanilla object)
         self.shapes = ["rect", "oval"]
         self.w.shape = RadioGroup((10, 70, -10, 22), self.shapes, isVertical=False)
         self.w.shape.set(0)
-        
+
         self.w.open()
-        
+
     def okCallback(self, sender):
         ## draw the shape in the glyph
         ## get the shape from the radio group
@@ -68,18 +68,18 @@ class GeometricShapesWindow(object):
             h = int(self.w.hInput.get())
         ## if this fails just do nothing and print a tiny traceback
         except:
-            print "A number is required!" 
+            print "A number is required!"
             return
         ## draw the shape with the callback given on init
         self.callback(shape, (x, y, w, h), self.glyph)
-    
+
     def cancelCallback(self, sender):
         ## do nothing :)
         pass
 
 def _roundPoint(x, y):
     return int(round(x)), int(round(y))
-     
+
 class DrawGeometricShapesTool(BaseEventTool):
 
     def setup(self):
@@ -89,6 +89,7 @@ class DrawGeometricShapesTool(BaseEventTool):
         self.maxPoint = None
         self.shape = "rect"
         self.origin = "corner"
+        self.moveShapeShift = None
 
     def getRect(self):
         ## return the rect between mouse down and mouse up
@@ -123,9 +124,9 @@ class DrawGeometricShapesTool(BaseEventTool):
         if h < 0:
             h = abs(h)
             y -= h
-        
+
         return x, y, w, h
-    
+
     def drawShapeWithRectInGlyph(self, shape, rect, glyph):
         ## draw the shape into the glyph
         ## tell the glyph something is going to happen (undo is going to be prepared)
@@ -133,11 +134,11 @@ class DrawGeometricShapesTool(BaseEventTool):
 
         ## get the pen to draw with
         pen = glyph.getPointPen()
-        if glyph.preferedSegmentType == "qcurve":
+        if glyph.preferredSegmentType == "qcurve":
             pen = ReverseContourPointPen(pen)
-        
+
         x, y, w, h = rect
-        
+
         ## draw with the pen a rect in the glyph
         if shape == "rect":
             pen.beginPath()
@@ -147,18 +148,18 @@ class DrawGeometricShapesTool(BaseEventTool):
             pen.addPoint(_roundPoint(x, y + h), "line")
 
             pen.endPath()
-        
+
         ## draw with the pen an oval in the glyph
         elif shape == "oval":
-            
+
             hw = w/2.
             hh = h/2.
-            
+
             r = .55
-            segmentType = glyph.preferedSegmentType
-            if glyph.preferedSegmentType == "qcurve":
+            segmentType = glyph.preferredSegmentType
+            if glyph.preferredSegmentType == "qcurve":
                 r = .42
-            
+
             pen.beginPath()
             pen.addPoint(_roundPoint(x + hw, y), segmentType, True)
             pen.addPoint(_roundPoint(x + hw + hw*r, y))
@@ -180,21 +181,26 @@ class DrawGeometricShapesTool(BaseEventTool):
 
         ## tell the glyph you are done with your actions so it can handle the undo properly
         glyph.performUndo()
-    
+
     def mouseDown(self, point, clickCount):
         ## a mouse down, only save the mouse down point
         self.minPoint = point
         ## on double click pop up an modal dialog with inputs
         if clickCount == 2:
             ## create and open the modal dialog
-            GeometricShapesWindow(self.getGlyph(), 
-                            callback=self.drawShapeWithRectInGlyph, 
-                            x=self.minPoint.x, 
+            GeometricShapesWindow(self.getGlyph(),
+                            callback=self.drawShapeWithRectInGlyph,
+                            x=self.minPoint.x,
                             y=self.minPoint.y)
 
-    def mouseDragged(self, point, delta):
+    def mouseDragged(self, point, delta):        
         ## record the draggin point
-        self.maxPoint = point        
+        self.maxPoint = point
+        ## if shift the minPoint by the move shift
+        if self.moveShapeShift:
+            w, h = self.moveShapeShift
+            self.minPoint.x = self.maxPoint.x - w
+            self.minPoint.y = self.maxPoint.y - h
 
     def mouseUp(self, point):
         ## mouse up, if you have recorded the rect draw that into the glyph
@@ -203,20 +209,28 @@ class DrawGeometricShapesTool(BaseEventTool):
         ## reset the tool
         self.minPoint = None
         self.maxPoint = None
-        
+
     def modifiersChanged(self):
         ## is been called when the modifiers changed (shift, alt, control, command)
         self.shape = "rect"
         self.origin = "corner"
+        
         ## change the shape when option is down
         if self.optionDown:
             self.shape = "oval"
         ## change the origin when command is down
         if self.commandDown:
             self.origin = "center"
+        ## record the current size of the shape and store it
+        if self.controlDown and self.moveShapeShift is None:
+            w = self.maxPoint.x - self.minPoint.x
+            h = self.maxPoint.y - self.minPoint.y
+            self.moveShapeShift = w, h
+        else:
+            self.moveShapeShift = None
         ## refresh the current glyph view
         self.getNSView().refresh()
-        
+
     def draw(self, scale):
         ## draw stuff in the current glyph view
         if self.isDragging() and self.minPoint and self.maxPoint:
@@ -226,11 +240,11 @@ class DrawGeometricShapesTool(BaseEventTool):
             rect = NSMakeRect(x, y, w, h)
             ## set the color
             NSColor.redColor().set()
-            
+
             if self.shape == "rect":
                 ## create a rect path
                 path = NSBezierPath.bezierPathWithRect_(rect)
-                
+
             elif self.shape == "oval":
                 ## create a oval path
                 path = NSBezierPath.bezierPathWithOvalInRect_(rect)
@@ -258,7 +272,7 @@ class DrawGeometricShapesTool(BaseEventTool):
             return _cursorRect
         else:
             return _cursorOval
-            
+
     def getToolbarIcon(self):
         ## return the toolbar icon
         return toolbarIcon
